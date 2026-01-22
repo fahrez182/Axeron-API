daffa0b0b70e30c0c2b979214a492db76bf71665
FahrezRB <fahreza.raditya.busan@gmail.com>
2026-01-22 13:35:03 +0700

    refactor: Change API version calculation logic
    This commit modifies the calculation for `apiVersionName` and `apiVersionCode` in `manifest.gradle.kts`.
    
    - **`apiVersionName`:** The patch version is now represented by only its first digit (e.g., `1.4.0` instead of `1.4.0`).
    - **`apiVersionCode`:** The calculation logic is updated to use a dynamic shift based on the number of digits in the patch version, ensuring a more consistent encoding.

--- manifest.gradle.kts
+++ manifest.gradle.kts
@@ -1,12 +1,21 @@
+import kotlin.math.pow
+
 val apiVersionMajor = 1
 val apiVersionMinor = 4
 val apiVersionPatch = 0
 
-val apiVersionName = "$apiVersionMajor.$apiVersionMinor.$apiVersionPatch"
-val apiVersionCode =
-    apiVersionMajor * 10_000 +
-            apiVersionMinor * 1_000 +
-            apiVersionPatch
+val patchMajorDigit = apiVersionPatch
+    .toString()
+    .first()
+    .digitToInt()
+
+val apiVersionName = "$apiVersionMajor.$apiVersionMinor.$patchMajorDigit"
+
+val base = apiVersionMajor * 10_000 + apiVersionMinor * 1_000
+val patchDigits = apiVersionPatch.toString().length
+val patchShift = 10.0.pow(3 - patchDigits).toInt()
+
+val apiVersionCode = base + apiVersionPatch * patchShift
 
 
 extra["api_version_major"] = apiVersionMajor
